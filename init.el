b66120ebb30e1111467b0dfa6178bc6c38f8a543
Hiroki Noda <kubo39@gmail.com>
2013-10-11 01:41:38 +0900

    flymake for python

--- init.el
+++ init.el
@@ -420,17 +420,17 @@
 
 
 ;;; python: flymake + pyflakes + pep8
-;; (add-hook 'find-file-hook 'flymake-find-file-hook)
-;;   (defun flymake-pyflakes-init ()
-;;     (let* ((temp-file (flymake-init-create-temp-buffer-copy
-;; 		       'flymake-create-temp-inplace))
-;; 	   (local-file (file-relative-name
-;; 			temp-file
-;; 			(file-name-directory buffer-file-name))))
-;;       (list "~/.emacs.d/pycheckers"  (list local-file))
-;;       ))
-;; (add-to-list 'flymake-allowed-file-name-masks
-;; 	     '("\\.py\\'" flymake-pyflakes-init))
+(add-hook 'find-file-hook 'flymake-find-file-hook)
+  (defun flymake-pyflakes-init ()
+    (let* ((temp-file (flymake-init-create-temp-buffer-copy
+		       'flymake-create-temp-inplace))
+	   (local-file (file-relative-name
+			temp-file
+			(file-name-directory buffer-file-name))))
+      (list "~/.emacs.d/pycheckers"  (list local-file))
+      ))
+(add-to-list 'flymake-allowed-file-name-masks
+	     '("\\.py\\'" flymake-pyflakes-init))
 
 ;;; jedi - python autocompletion
 (autoload 'jedi:setup "jedi" nil t)
