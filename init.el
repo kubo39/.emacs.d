1f3c49631a180948ea33fbe47d515742c3bd9b2d
Hiroki Noda <kubo39@gmail.com>
2014-05-19 18:15:32 +0900

    Fxxk:disabled markdown-mode

--- init.el
+++ init.el
@@ -481,10 +481,10 @@
 
 
 ;; markdown-mode
-(require 'markdown-mode)
+;(require 'markdown-mode)
 
-(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+;(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+;(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
 
 ;; =====================================================
