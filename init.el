44cd7fd06cd3033ddee233510cfae01fedcf9b39
kubo39 <kubo39@gmail.com>
2015-03-08 01:16:52 +0900

    auto-installまわりの設定削除

--- init.el
+++ init.el
@@ -87,11 +87,6 @@
    :foreground "red"
    :box nil)
 
-;;; auto-install.el
-;; (when (require 'auto-install nil t)
-;;   (setq auto-install-directory "~/.emacs.d/elisp/")
-;;   (auto-install-update-emacswiki-package-name t)
-;  ; (auto-install-compatibility-setup))
 
 ;;; ターミナルエミュレータのシェルを bash に設定
 ;; (when (require 'multi-term nil t)
