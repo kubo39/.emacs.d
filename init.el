0dabf6b6e2e3d7145a68de53d1ec671eccb567ee
kubo39 <kubo39@gmail.com>
2015-05-14 00:56:25 +0900

    flycheck デフォルトオンに

--- init.el
+++ init.el
@@ -224,6 +224,7 @@
 (eval-after-load 'flycheck
   '(custom-set-variables
     '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages)))
+(add-hook 'after-init-hook #'global-flycheck-mode)
 
 ;; =====================================================
 ;;
