2c36b5746194a5c408e1dc99e1623f77531047ad
kubo39 <kubo39@gmail.com>
2015-08-07 02:22:58 +0900

    Add ponylang-mode

--- init.el
+++ init.el
@@ -350,3 +350,10 @@
 
 ;; crystal
 (add-to-list 'auto-mode-alist '("\\.cr$" . ruby-mode))
+
+
+(add-hook
+  'ponylang-mode-hook
+  (lambda ()
+    (set-variable 'indent-tabs-mode nil)
+    (set-variable 'tab-width 2)))
