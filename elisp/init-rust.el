b7e2c883934e05b733f52e005dd207b813e82197
kubo39 <kubo39@gmail.com>
2015-11-29 04:12:30 +0900

    Fix path.

--- elisp/init-rust.el
+++ elisp/init-rust.el
@@ -3,8 +3,8 @@
 (add-to-list 'auto-mode-alist '("\\.rs$" . rust-mode))
 
 ;; racer -- auto-compelte for rust
-(setq racer-rust-src-path "/home/kubo39/rust/src/")
-(setq racer-cmd "/home/kubo39/racer/target/release/racer")
-(add-to-list 'load-path "/home/kubo39/racer/editors")
+(setq racer-rust-src-path "~/rust/src/")
+(setq racer-cmd "~/racer/target/release/racer")
+(add-to-list 'load-path "/~/racer/editors")
 
 (provide 'init-rust)
