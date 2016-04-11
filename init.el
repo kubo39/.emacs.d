dcdb579112758a8fad49d2b88272ebaa00b5380b
kubo39 <kubo39@gmail.com>
2016-04-11 18:55:38 +0900

    typescriptの設定

--- init.el
+++ init.el
@@ -146,6 +146,11 @@
 
 ;-------------------------------------------------------
 
+;; typescript
+(require 'typescript-mode)
+(add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-mode))
+(require 'tss)
+(tss-config-default)
 
 
 ;; 言語系
