f2bc1b7df53ec64d6c8c5b3b5f12058cefda1d56
kubo39 <kubo39@gmail.com>
2016-04-23 18:26:06 +0900

    Using dstyle.

--- elisp/init-d.el
+++ elisp/init-d.el
@@ -8,14 +8,14 @@
 (setq auto-mode-alist (cons '("\\.d$" . d-mode) auto-mode-alist))
 (setup-flycheck-d-unittest)
 (setq load-path (cons "~/DCD/bin" load-path)) ;;;   DCDに load-path を通す
-(require 'ac-dcd)                             ;;; ac-dcd
+;; (require 'ac-dcd)                             ;;; ac-dcd
 (add-hook 'd-mode-hook
           '(lambda ()
              (c-set-style "bsd")
-             (setq c-basic-offset 2)
+             (setq c-basic-offset 4)
              (setq c-auto-newline t)
              (setq indent-tabs-mode nil)
-             (setq tab-width 2)
+             (setq tab-width 4)
              (local-set-key  (kbd "C-c C-p") 'flycheck-previous-error)
              (local-set-key  (kbd "C-c C-n") 'flycheck-next-error)
              (ac-dcd-setup)))
