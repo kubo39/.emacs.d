7fe5775d18350bc13c4e81719c1a0eb5dc811208
Hiroki Noda <kubo39@gmail.com>
2016-05-15 22:11:19 +0900

    Update rust-mode.

--- elisp/init-rust.el
+++ elisp/init-rust.el
@@ -4,14 +4,20 @@
 (add-to-list 'auto-mode-alist '("\\.rs$" . rust-mode))
 
 ;; racer -- auto-compelte for rust
+(setq racer-cmd "~/.cargo/bin/racer")
 (setq racer-rust-src-path "~/rust/src/")
-(setq racer-cmd "~/racer/target/release/racer")
-(add-to-list 'load-path "/~/racer/editors")
 
 (add-hook 'rust-mode-hook
-  '(lambda ()
-     (racer-activate)
-     (local-set-key (kbd "M-.") #'racer-find-definition)
-     (local-set-key (kbd "TAB") #'racer-complete-or-indent)))
+          '(lambda ()
+             (cargo-minor-mode)
+             (racer-mode)
+             (local-set-key (kbd "C-c <tab>") #'rust-format-buffer)))
+
+(add-hook 'racer-mode
+          '(lambda ()
+             (eldoc-mode)
+             (company-mode)))
+
+(add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
 
 (provide 'init-rust)
