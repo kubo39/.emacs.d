1d43b1a7424f947c3f0a21d3d7c095b15bc6258e
Hiroki Noda <kubo39@gmail.com>
2016-06-06 04:36:47 +0900

    Up.

--- elisp/init-rust.el
+++ elisp/init-rust.el
@@ -11,13 +11,13 @@
           '(lambda ()
              (cargo-minor-mode)
              (racer-mode)
-             (local-set-key (kbd "C-c <tab>") #'rust-format-buffer)))
+             (local-set-key (kbd "C-c <tab>") 'rust-format-buffer)))
 
 (add-hook 'racer-mode
           '(lambda ()
              (eldoc-mode)
              (company-mode)))
 
-(add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
+;; (add-hook 'flycheck-mode-hook 'flycheck-rust-setup)
 
 (provide 'init-rust)
