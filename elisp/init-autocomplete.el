24659ba4a00faa14a4815e54c289e08f4f6e8566
Hiroki Noda <kubo39@gmail.com>
2016-11-05 15:06:06 +0900

    Add ac-etags.

--- elisp/init-autocomplete.el
+++ elisp/init-autocomplete.el
@@ -5,4 +5,13 @@
 (require 'auto-complete)
 (global-auto-complete-mode t)
 
+(require 'ac-etags)
+
+(custom-set-variables
+  '(ac-etags-requires 1))
+
+(eval-after-load "etags"
+  '(progn
+      (ac-etags-setup)))
+
 (provide 'init-autocomplete)
