f3e610557eddf09998618bca3dfe82b45fbe93aa
Hiroki Noda <kubo39@gmail.com>
2016-11-07 03:15:23 +0900

    Minor fix.

--- elisp/init-d.el
+++ elisp/init-d.el
@@ -5,9 +5,11 @@
 
 (require 'd-mode)
 (autoload 'd-mode "d-mode" "Major mode for editing D code." t)
+
 (setq auto-mode-alist (cons '("\\.d$" . d-mode) auto-mode-alist))
 (setup-flycheck-d-unittest)
 (setq load-path (cons "~/DCD/bin" load-path)) ;;;   DCDに load-path を通す
+
 (require 'ac-dcd)                             ;;; ac-dcd
 (add-hook 'd-mode-hook
           '(lambda ()
@@ -18,7 +20,7 @@
              (setq tab-width 4)
              (local-set-key  (kbd "C-c C-p") 'flycheck-previous-error)
              (local-set-key  (kbd "C-c C-n") 'flycheck-next-error)
-             ('ac-etags-ac-setup)
+             (ac-etags-ac-setup)
              (ac-dcd-setup)))
 
 (provide 'init-d)
