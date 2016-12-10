7d3c3b66a94f0dbc2db0a2a10d1c5e1bd6d4a6dc
Hiroki Noda <kubo39@gmail.com>
2016-12-10 21:07:13 +0900

    Remove flycheck-d-unittest

--- elisp/init-d.el
+++ elisp/init-d.el
@@ -7,7 +7,6 @@
 (autoload 'd-mode "d-mode" "Major mode for editing D code." t)
 
 (setq auto-mode-alist (cons '("\\.d$" . d-mode) auto-mode-alist))
-(setup-flycheck-d-unittest)
 (setq load-path (cons "~/DCD/bin" load-path)) ;;;   DCDに load-path を通す
 
 (require 'ac-dcd)                             ;;; ac-dcd
@@ -17,8 +16,6 @@
              (setq c-basic-offset 4)
              (setq indent-tabs-mode nil)
              (setq tab-width 4)
-             (local-set-key  (kbd "C-c C-p") 'flycheck-previous-error)
-             (local-set-key  (kbd "C-c C-n") 'flycheck-next-error)
              (ac-etags-ac-setup)
              (ac-dcd-setup)))
 
