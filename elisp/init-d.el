b63ecb161296a1777715e73e867acb5ec83266dc
Hiroki Noda <kubo39@gmail.com>
2017-05-01 15:30:39 +0900

    D loves java-mode

--- elisp/init-d.el
+++ elisp/init-d.el
@@ -3,20 +3,20 @@
 ;;;-------------------------------------
 
 
-(require 'd-mode)
-(autoload 'd-mode "d-mode" "Major mode for editing D code." t)
+;; (require 'd-mode)
+;; (autoload 'd-mode "d-mode" "Major mode for editing D code." t)
 
-(setq auto-mode-alist (cons '("\\.d$" . d-mode) auto-mode-alist))
-(setq load-path (cons "~/DCD/bin" load-path)) ;;;   DCDに load-path を通す
+(setq auto-mode-alist (cons '("\\.d$" . java-mode) auto-mode-alist))
+;; (setq load-path (cons "~/DCD/bin" load-path)) ;;;   DCDに load-path を通す
 
-(require 'ac-dcd)                             ;;; ac-dcd
-(add-hook 'd-mode-hook
-          '(lambda ()
-             (c-set-style "bsd")
-             (setq c-basic-offset 4)
-             (setq indent-tabs-mode nil)
-             (setq tab-width 4)
-             (ac-etags-ac-setup)
-             (ac-dcd-setup)))
+;; (require 'ac-dcd)                             ;;; ac-dcd
+;; (add-hook 'd-mode-hook
+;;           '(lambda ()
+;;              (c-set-style "bsd")
+;;              (setq c-basic-offset 4)
+;;              (setq indent-tabs-mode nil)
+;;              (setq tab-width 4)
+;;              (ac-etags-ac-setup)
+;;              (ac-dcd-setup)))
 
 (provide 'init-d)
