6e1ff0b0d32b2e175db598f04885a92ee7ba17ea
Hiroki Noda <kubo39@gmail.com>
2017-10-04 14:48:51 +0900

    Use d-mode with company-dcd

--- elisp/init-d.el
+++ elisp/init-d.el
@@ -2,21 +2,17 @@
 ;;; D-Language
 ;;;-------------------------------------
 
+(require 'd-mode)
 
-;; (require 'd-mode)
-;; (autoload 'd-mode "d-mode" "Major mode for editing D code." t)
+(setq auto-mode-alist (cons '("\\.d$" . d-mode) auto-mode-alist))
+(setq load-path (cons "~/DCD/bin" load-path)) ;;;   DCDに load-path を通す
 
-(setq auto-mode-alist (cons '("\\.d$" . java-mode) auto-mode-alist))
-;; (setq load-path (cons "~/DCD/bin" load-path)) ;;;   DCDに load-path を通す
-
-;; (require 'ac-dcd)                             ;;; ac-dcd
-;; (add-hook 'd-mode-hook
-;;           '(lambda ()
-;;              (c-set-style "bsd")
-;;              (setq c-basic-offset 4)
-;;              (setq indent-tabs-mode nil)
-;;              (setq tab-width 4)
-;;              (ac-etags-ac-setup)
-;;              (ac-dcd-setup)))
+(require 'company-dcd)
+(add-hook 'd-mode-hook
+          (lambda ()
+            (c-set-style "bsd")
+            (setq c-basic-offset 4)
+            (setq indent-tabs-mode nil)
+            (setq tab-width 4)))
 
 (provide 'init-d)
