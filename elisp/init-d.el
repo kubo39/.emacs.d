ea43d9f46efd09920b0c4ce2bddc686e62a36d4a
Hiroki Noda <kubo39@gmail.com>
2017-11-08 02:27:45 +0900

    Add dfmt

--- elisp/init-d.el
+++ elisp/init-d.el
@@ -6,15 +6,19 @@
 
 (setq auto-mode-alist (cons '("\\.d$" . d-mode) auto-mode-alist))
 
-(add-to-list 'exec-path "~/dlang/dmd-2.076.1/linux/bin64/")
+(add-to-list 'exec-path "~/dlang/dmd-2.077.0/linux/bin64/")
+(add-to-list 'exec-path "~/.dub/packages/dfmt-master/dfmt/")
 (add-to-list 'exec-path "~/DCD/bin/")
 
 (require 'company-dcd)
+
 (add-hook 'd-mode-hook
           (lambda ()
             (c-set-style "bsd")
             (setq c-basic-offset 4)
             (setq indent-tabs-mode nil)
-            (setq tab-width 4)))
+            (setq tab-width 4)
+            ;; (company-dcd-mode)
+            (dfmt-setup-keys)))
 
 (provide 'init-d)
--- init.el
+++ init.el
@@ -41,7 +41,8 @@
     ;; d
     d-mode
     company-dcd
-    flycheck-d-unittest
+    dfmt
+    ;;; flycheck-d-unittest
     ;; elm
     elm-mode
     ;; rust
@@ -253,7 +254,7 @@
  '(ac-etags-requires 1)
  '(package-selected-packages
    (quote
-    (idris-mode flymake-hlint company-dcd flycheck-elm elm-mode haskell-mode bison-mode editorconfig dockerfile-mode erlang ponylang-mode toml-mode typescript-mode tss rustfmt moe-theme powerline tabbar smex popwin el-get company browse-kill-ring auto-save-buffers-enhanced))))
+    (dfmt idris-mode flymake-hlint company-dcd flycheck-elm elm-mode haskell-mode bison-mode editorconfig dockerfile-mode erlang ponylang-mode toml-mode typescript-mode tss rustfmt moe-theme powerline tabbar smex popwin el-get company browse-kill-ring auto-save-buffers-enhanced))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
