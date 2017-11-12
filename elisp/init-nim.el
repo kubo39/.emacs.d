84ece52d040dce418a4f4d872d6647b82f714667
Hiroki Noda <kubo39@gmail.com>
2017-11-12 18:34:52 +0900

    Update nim config

--- elisp/init-nim.el
+++ elisp/init-nim.el
@@ -1,6 +1,10 @@
 ;; nim-mode
 
+(setq nimsuggest-path "~/.nimble/bin/nimsuggest")
+
 (require 'nim-mode)
+
 (add-hook 'nim-mode-hook 'nimsuggest-mode)
+(add-hook 'nimsuggest-mode-hook 'company-mode)
 
 (provide 'init-nim)
