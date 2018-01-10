7542e1579dc1a0e75a4b715bddc50996d4686ad1
Hiroki Noda <kubo39@gmail.com>
2018-01-10 21:38:41 +0900

    Commented out DCD since always SEGV occurs

--- elisp/init-d.el
+++ elisp/init-d.el
@@ -19,19 +19,19 @@
 
 (add-to-list 'exec-path "~/dlang/dmd-2.078.0/linux/bin64/")
 (add-to-list 'exec-path "~/.dub/packages/dfmt-master/dfmt/")
-(add-to-list 'exec-path "~/DCD/bin/")
+;; (add-to-list 'exec-path "~/DCD/bin/")
 
-(require 'company-dcd)
+;; (require 'company-dcd)
 
 (add-hook 'd-mode-hook
           (lambda ()
             (c-set-style "bsd")
             (setq c-basic-offset 4)
             (setq tab-width 4)
-            (company-dcd-mode)
+            ;; (company-dcd-mode)
             (dfmt-setup-keys)
-            (define-key company-dcd-mode-map (kbd "M-.") 'company-dcd-goto-definition)
-            (define-key company-dcd-mode-map (kbd "M-,") 'company-dcd-goto-def-pop-marker)
+            ;; (define-key company-dcd-mode-map (kbd "M-.") 'company-dcd-goto-definition)
+            ;; (define-key company-dcd-mode-map (kbd "M-,") 'company-dcd-goto-def-pop-marker)
             (local-set-key (kbd "C-c C-f") 'dfmt-buffer)))
 
 (provide 'init-d)
