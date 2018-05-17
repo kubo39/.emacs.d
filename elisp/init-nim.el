4b405bbb9ddbec69d95c83dea713637856b01bbf
Hiroki Noda <kubo39@gmail.com>
2018-05-17 16:03:57 +0900

    So long, nimsuggest

--- elisp/init-nim.el
+++ elisp/init-nim.el
@@ -1,10 +1,10 @@
 ;; nim-mode
 
-(setq nimsuggest-path "~/.nimble/bin/nimsuggest")
+;(setq nimsuggest-path "~/.nimble/bin/nimsuggest")
 
 (require 'nim-mode)
 
-(add-hook 'nim-mode-hook 'nimsuggest-mode)
-(add-hook 'nimsuggest-mode-hook 'company-mode)
+;(add-hook 'nim-mode-hook 'nimsuggest-mode)
+;(add-hook 'nimsuggest-mode-hook 'company-mode)
 
 (provide 'init-nim)
