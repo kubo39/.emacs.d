c7811cce1b6fcf3259104abc0b626cb8f9825288
Hiroki Noda <kubo39@gmail.com>
2018-09-13 00:36:55 +0900

    Up

--- elisp/init-pony.el
+++ elisp/init-pony.el
@@ -1,8 +1,15 @@
-;;; ponylang
+;;; init-pony --- Settings for editing Pony source code
+
+;;; Commentary:
+;;
+
+
+
+;;; Code:
 
 (require 'ponylang-mode)
 (autoload 'ponylang-mode "ponylang-mode" "Major mode for editing Pony code." t)
 (setq auto-mode-alist (cons '("\\.pony$" . ponylang-mode) auto-mode-alist))
 
 (provide 'init-pony)
-
+;;; init-pony.el ends here
