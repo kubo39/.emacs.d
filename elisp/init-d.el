7e120e617ed2d7dbd73c94a3a849b62db84218f7
Hiroki Noda <kubo39@gmail.com>
2018-11-05 22:13:22 +0900

    Update dmd

--- elisp/init-d.el
+++ elisp/init-d.el
@@ -17,7 +17,7 @@
 
 (require 'd-mode)
 
-(add-to-list 'exec-path "~/dlang/dmd-2.082.1/linux/bin64/")
+(add-to-list 'exec-path "~/dlang/dmd-2.083.0/linux/bin64/")
 (add-to-list 'exec-path "~/.dub/packages/dfmt-master/dfmt/")
 (add-to-list 'exec-path "~/DCD/bin/")
 
