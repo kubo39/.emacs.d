773f27acdec36ff67e536ab89e6f9c721802582c
Hiroki Noda <kubo39@gmail.com>
2020-03-07 16:07:39 +0900

    add quickrun

--- init.el
+++ init.el
@@ -61,7 +61,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (nim-mode haskell-mode d-mode python-mode gnu-elpa-keyring-update company-coq fsharp-mode exec-path-from-shell review-mode ghc jedi flymake-hlint flycheck-elm elm-mode bison-mode editorconfig dockerfile-mode toml-mode moe-theme powerline tabbar smex popwin company browse-kill-ring)))
+    (quickrun nim-mode haskell-mode d-mode python-mode gnu-elpa-keyring-update company-coq fsharp-mode exec-path-from-shell review-mode ghc jedi flymake-hlint flycheck-elm elm-mode bison-mode editorconfig dockerfile-mode toml-mode moe-theme powerline tabbar smex popwin company browse-kill-ring)))
  '(ruby-insert-encoding-magic-comment nil)
  '(safe-local-variable-values (quote ((whitespace-line-column . 80)))))
 
@@ -288,6 +288,10 @@
 ;; 非アクティブなウィンドウを暗くする
 (require 'dimmer)
 (dimmer-mode)
+
+;;; quickrun
+(use-package quickrun
+  :ensure t)
 
 ;-------------------------------------------------------
 
