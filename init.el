e2ab0d890f0e6e65afba288afcbe21b783d4e9c1
Hiroki Noda <kubo39@gmail.com>
2020-03-07 15:14:42 +0900

    remove elisp/init-elpa

--- init.el
+++ init.el
@@ -11,7 +11,9 @@
 
 (add-to-list 'load-path (expand-file-name "elisp" user-emacs-directory))
 
-(require 'init-elpa)
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.milkbox.net/packages/") t) ;; meplaを追加
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/") t) ;; elpaを追加
 (package-initialize)
 
 (when (memq window-system '(mac ns x))
