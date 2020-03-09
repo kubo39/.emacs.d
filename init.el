c122414256325cf4de6d5376f31e87fd6acb910c
Hiroki Noda <kubo39@gmail.com>
2020-03-10 06:40:19 +0900

    Fix: do not add list elpa twice

--- init.el
+++ init.el
@@ -13,7 +13,6 @@
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.milkbox.net/packages/") t) ;; meplaを追加
-(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/") t) ;; elpaを追加
 (package-initialize)
 
 (when (memq window-system '(mac ns x))
