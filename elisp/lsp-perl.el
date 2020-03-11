842b729ea096d0007d52c0f9329d9547c5183a27
Hiroki Noda <kubo39@gmail.com>
2020-03-12 08:19:24 +0900

    Fixup

--- elisp/lsp-perl.el
+++ elisp/lsp-perl.el
@@ -13,30 +13,30 @@
   :group 'lsp-mode
   :link '(url-link "https://github.com/richterger/Perl-LanguageServer"))
 
-(defcustom lsp-perl-path "perl"
+(defcustom lsp-perl-language-server-path "perl"
   "Path to perl interpreter."
   :type 'string
   :group 'lsp-perl)
 
-(defcustom lsp-perl-server-port 13603
-  ""
+(defcustom lsp-perl-language-server-port 13603
+  "Listen port."
   :type 'integer
   :group 'lsp-perl)
 
-(defcustom lsp-perl-client-version "2.1.0"
-  ""
+(defcustom lsp-perl-language-server-client-version "2.1.0"
+  "client version."
   :type 'string
   :group 'lsp-perl)
 
 (lsp-register-client
  (make-lsp-client :new-connection (lsp-stdio-connection
                                    (lambda ()
-                                     (list lsp-perl-path
+                                     (list lsp-perl-language-server-path
                                            (format "-MPerl::LanguageServer -e Perl::LanguageServer::run -- --port %o --version %s"
-                                                   lsp-perl-server-port lsp-perl-client-version))))
+                                                   lsp-perl-language-server-port lsp-perl-language-server-client-version))))
                   :major-modes '(perl-mode)
                   :priority -1
-                  :server-id 'perl-lang-server))
+                  :server-id 'perl-language-server))
 
 (provide 'lsp-perl)
 ;;; lsp-perl.el ends here
