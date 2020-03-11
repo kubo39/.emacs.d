37e7766f836e41775938fbd1c290e3c8e724addf
Hiroki Noda <kubo39@gmail.com>
2020-03-12 08:14:42 +0900

    lsp-perl: allow to custom server-port and lsp-client-version

--- elisp/lsp-perl.el
+++ elisp/lsp-perl.el
@@ -11,19 +11,29 @@
 (defgroup lsp-perl nil
   "LSP support for Perl"
   :group 'lsp-mode
-  :link '(url-link "https://github.com/richterger/Perl-LangServer")
-  )
+  :link '(url-link "https://github.com/richterger/Perl-LangServer"))
 
 (defcustom lsp-perl-path "perl"
   "Path to perl interpreter."
   :type 'string
   :group 'lsp-perl)
 
+(defcustom lsp-perl-server-port 13603
+  ""
+  :type 'integer
+  :group 'lsp-perl)
+
+(defcustom lsp-perl-client-version "2.1.0"
+  ""
+  :type 'string
+  :group 'lsp-perl)
+
 (lsp-register-client
  (make-lsp-client :new-connection (lsp-stdio-connection
                                    (lambda ()
-                                     (cons lsp-perl-path
-                                           '("-MPerl::LanguageServer -e Perl::LanguageServer::run -- --port 13603 --version 2.1.0"))))
+                                     (list lsp-perl-path
+                                           (format "-MPerl::LanguageServer -e Perl::LanguageServer::run -- --port %o --version %s"
+                                                   lsp-perl-server-port lsp-perl-client-version))))
                   :major-modes '(perl-mode)
                   :priority -1
                   :server-id 'perl-lang-server))
