f75be26c8f8b27a3b1cd957826d6df16155d1aab
Hiroki Noda <kubo39@gmail.com>
2020-10-25 16:31:12 +0900

    Update ldc compiler version

--- init.el
+++ init.el
@@ -368,7 +368,7 @@
 (use-package d-mode
   :ensure t
   :init
-  (setq company-dcd-compiler "~/dlang/ldc-1.23.0/bin/ldc2")
+  (setq company-dcd-compiler "~/dlang/ldc-1.24.0/bin/ldc2")
   (setq company-dcd-client-executable "~/.dub/packages/dcd-0.12.0/dcd/bin/dcd-client")
   (setq company-dcd-server-executable "~/.dub/packages/dcd-0.12.0/dcd/bin/dcd-server")
   :hook
