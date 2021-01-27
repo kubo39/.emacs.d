c45158580a092a104f94c008d967df4512ab0c50
Hiroki Noda <kubo39@gmail.com>
2021-01-28 01:50:09 +0900

    dcdのアップデート

--- init.el
+++ init.el
@@ -371,8 +371,8 @@
   :ensure t
   :init
   (setq company-dcd-compiler "~/dlang/ldc-1.24.0/bin/ldc2")
-  (setq company-dcd-client-executable "~/.dub/packages/dcd-0.12.0/dcd/bin/dcd-client")
-  (setq company-dcd-server-executable "~/.dub/packages/dcd-0.12.0/dcd/bin/dcd-server")
+  (setq company-dcd-client-executable "~/.dub/packages/dcd-0.13.1/dcd/bin/dcd-client")
+  (setq company-dcd-server-executable "~/.dub/packages/dcd-0.13.1/dcd/bin/dcd-server")
   :hook
   (d-mode . (lambda ()
               (c-set-style "bsd")
