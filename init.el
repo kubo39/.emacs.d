c1c0373538a25bd0d0e9cdbb34687f8acce14b13
Hiroki Noda <kubo39@gmail.com>
2021-08-18 22:06:40 +0900

    update ldc

--- init.el
+++ init.el
@@ -367,7 +367,7 @@
 (use-package d-mode
   :ensure t
   :init
-  (setq company-dcd-compiler "~/dlang/ldc-1.27.0/bin/ldc2")
+  (setq company-dcd-compiler "~/dlang/ldc-1.27.1/bin/ldc2")
   (setq company-dcd-client-executable "~/.dub/packages/dcd-0.13.1/dcd/bin/dcd-client")
   (setq company-dcd-server-executable "~/.dub/packages/dcd-0.13.1/dcd/bin/dcd-server")
   :hook
